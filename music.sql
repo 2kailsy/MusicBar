cff774342d727b28672aa84c50e4eb35f1d626dd
2kdtll <62921455+2kailsy@users.noreply.github.com>
2025-01-30 22:04:21 +0800

    Update music.sql

--- music.sql
+++ music.sql
@@ -1,44 +1,125 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- 数据库： `music`
+--
+
 -- --------------------------------------------------------
--- 主机:                           127.0.0.1
--- 服务器版本:                        5.5.62-log - MySQL Community Server (GPL)
--- 服务器操作系统:                      Win64
--- HeidiSQL 版本:                  11.3.0.6295
+
+--
+-- 表的结构 `list`
+--
+
+CREATE TABLE `list` (
+  `id` int(11) NOT NULL,
+  `listName` varchar(200) NOT NULL DEFAULT '我的收藏',
+  `uid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 -- --------------------------------------------------------
 
--- 导出 music 的数据库结构
-CREATE DATABASE IF NOT EXISTS `music`
-USE `music`;
+--
+-- 表的结构 `music`
+--
 
--- 导出  表 music.list 结构
-CREATE TABLE IF NOT EXISTS `list` (
-  `listName` varchar(50) DEFAULT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+CREATE TABLE `music` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL COMMENT '歌曲名',
+  `author` varchar(300) NOT NULL COMMENT '作者',
+  `pic` varchar(500) NOT NULL COMMENT '歌曲封面',
+  `rid` int(11) NOT NULL COMMENT '歌曲id',
+  `uid` int(11) NOT NULL COMMENT '用户id',
+  `lid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='歌曲收藏';
 
--- 导出  表 music.music 结构
-CREATE TABLE IF NOT EXISTS `music` (
-  `uid` int(11) DEFAULT NULL,
-  `lid` int(11) DEFAULT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  `author` varchar(50) DEFAULT NULL,
-  `rid` int(11) DEFAULT NULL,
-  `pic` varchar(500) DEFAULT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+-- --------------------------------------------------------
 
--- 导出  表 music.users 结构
-CREATE TABLE IF NOT EXISTS `users` (
-  `mail` varchar(50) DEFAULT NULL,
-  `password` varchar(150) DEFAULT NULL,
-  `time` datetime DEFAULT NULL,
-  `img` varchar(150) DEFAULT NULL,
-  `sex` int(11) DEFAULT NULL,
-  `sign` varchar(50) DEFAULT NULL,
-  `plays` int(11) DEFAULT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+--
+-- 表的结构 `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(200) DEFAULT NULL COMMENT '用户名',
+  `mail` varchar(200) NOT NULL COMMENT '邮箱',
+  `password` varchar(200) NOT NULL COMMENT '密码',
+  `img` varchar(500) DEFAULT NULL COMMENT '头像',
+  `sex` int(11) DEFAULT '2' COMMENT '性别(0:男,1:女,其他)',
+  `sign` varchar(500) DEFAULT '这个人很懒喔，什么东东都没有' COMMENT '个人简介',
+  `plays` int(11) DEFAULT '0' COMMENT '听过几次歌',
+  `time` datetime NOT NULL COMMENT '注册时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 转储表的索引
+--
+
+--
+-- 表的索引 `list`
+--
+ALTER TABLE `list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `uid` (`uid`);
+
+--
+-- 表的索引 `music`
+--
+ALTER TABLE `music`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `uid` (`uid`),
+  ADD KEY `lid` (`lid`);
+
+--
+-- 表的索引 `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `mail` (`mail`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `list`
+--
+ALTER TABLE `list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- 使用表AUTO_INCREMENT `music`
+--
+ALTER TABLE `music`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- 使用表AUTO_INCREMENT `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- 限制导出的表
+--
+
+--
+-- 限制表 `list`
+--
+ALTER TABLE `list`
+  ADD CONSTRAINT `list_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users` (`id`);
+
+--
+-- 限制表 `music`
+--
+ALTER TABLE `music`
+  ADD CONSTRAINT `music_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `music_ibfk_2` FOREIGN KEY (`lid`) REFERENCES `list` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
